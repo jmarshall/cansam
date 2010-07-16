109a92f2b583cc0bba0c82ba8e923882f1c49a21
John Marshall <john.marshall@sanger.ac.uk>
2010-07-16 14:49:48 +0100

    Implement bad_format::what() message

--- lib/exception.cpp
+++ lib/exception.cpp
@@ -1,13 +1,35 @@
 #include "sam/exception.h"
 
+#include <sstream>
 #include <cstring>  // for strerror()
 
 namespace sam {
 
+namespace {
+
+// Returns whether S ends in a non-space character, i.e., is a complete phrase.
+bool complete_phrase(const std::string& s) {
+  return (! s.empty()) && s[s.length() - 1] != ' ';
+}
+
+} // unnamed namespace
+
+// Returns "message [[for ] "filename"] [[at record] N]"
 const char* bad_format::what() const throw() {
-  // FIXME
-  what_text_ = sam::exception::what();
-  what_text_ += " (bad_format)";
+  std::ostringstream s;
+
+  s << sam::exception::what();
+  if (! filename().empty()) {
+    if (complete_phrase(s.str()))  s << " for ";
+    s << '"' << filename() << '"';
+  }
+
+  if (recnum() != 0) {
+    if (complete_phrase(s.str()))  s << " at record ";
+    s << recnum();
+  }
+
+  what_text_ = s.str();
   return what_text_.c_str();
 }
 
@@ -16,8 +38,7 @@ const char* system_error::what() const throw() {
   what_text_ = sam::exception::what();
 
   if (! filename().empty()) {
-    if (! (what_text_.empty() || what_text_[what_text_.length() - 1] == ' '))
-      what_text_ += " for ";
+    if (complete_phrase(what_text_))  what_text_ += " for ";
     what_text_ += '"';
     what_text_ += filename();
     what_text_ += '"';
