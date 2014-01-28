f796667c88d9ddd2994ce0c7348561daf8e46c9d
John Marshall <john.marshall@sanger.ac.uk>
2014-01-28 20:40:27 +0000

    Add missing <streambuf> inclusion
    This fixes compilation with recent versions of clang.

--- lib/sambamio.h
+++ lib/sambamio.h
@@ -1,6 +1,6 @@
 /*  sambamio.h -- SAM/BAM input/output formatting.
 
-    Copyright (C) 2010-2012 Genome Research Ltd.
+    Copyright (C) 2010-2014 Genome Research Ltd.
 
     Author: John Marshall <jm18@sanger.ac.uk>
 
@@ -30,6 +30,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  */
 #ifndef SAMBAMIO_H
 #define SAMBAMIO_H
 
+#include <streambuf>
 #include <vector>
 
 #include "cansam/sam/stream.h"
